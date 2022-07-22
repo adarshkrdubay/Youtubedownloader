ddf8d3f7aa4605574b874f33c2797ccfb1815ed9
Adarsh kumar <akdubay45@gmail.com>
2022-07-22 15:57:56 +0530

    Removed some code
    High quality removed .

--- Downloader2.py
+++ Downloader2.py
@@ -6,7 +6,6 @@ yt = YouTube(link)
 print("""Choose the Qualitiy:
 Press 1 for low quality.
 press 2 for medium quality .
-press 3 for high quality.
 """)
 a=input(":  ")
 if a=="1":
@@ -33,28 +32,6 @@ if a=="1":
 
          os.system("cls")
 elif a=="2":
-    print("""what do you want to download 
-    input a for audio 
-    input v for video """)
-    p=input(":  ")
-    if  p=="a" or p=="A":
-         z=input("input the name of file plz include .mp3 at last:  ")  
-         print(f'Downloding: {yt.title}')
-         yt.streams.first().download()
-         os.rename(yt.streams.first().default_filename, z )
-         print("DONE")
-         os.system("cls")
-    elif p=="V" or p=="v":
-         z=input("input the name of file plz include .mp4 at last:   ")
-         print(f'Downloding: {yt.title}')     
-         yt.streams.get_by_itag(137).download()
-         os.rename(yt.streams.get_by_itag(137).default_filename, z )
-         print("DONE")
-         os.system("cls")
-    else:
-         print("Wrong cammand")
-         os.system("cls")
-elif a=="3":
     print("""what do you want to download 
     input a for audio 
     input v for video """)
